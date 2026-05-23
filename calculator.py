42fdcdb43e4d81aa97dca0ea0b39408f330ce865
MR.K <ilakoskarov646@gmail.com>
2026-05-23 14:32:33 +0300

    Изменение вывода в feature ветке

--- calculator.py
+++ calculator.py
@@ -21,4 +21,4 @@ if __name__ == "__main__":
     print(f"2 + 3 = {add(2, 3)}")
     print(f"5 - 2 = {subtract(5, 2)}")
     print(f"4 * 6 = {multiply(4, 6)}")
-    print(f"10 / 2 = {divide(10, 2)}")
+    print(f"10 / 2 = {divide(10, 2)}")  # Изменено в feature/add-tests
